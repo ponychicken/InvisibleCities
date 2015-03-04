1d8038cf84c1726379e945cef5492c48cdd25f98
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-03-04 22:50:20 +0100

    Add back button

--- InvisibleCities/WebViewController.swift
+++ InvisibleCities/WebViewController.swift
@@ -22,8 +22,6 @@ let π = M_PI
 
 
 class WebViewController: UIViewController, UIGestureRecognizerDelegate, UINavigationBarDelegate, WKNavigationDelegate {
-
-    internal var url = startURL;
     
     @IBOutlet var contentView: UIView!
     @IBOutlet var containerView : UIView!
@@ -34,9 +32,16 @@ class WebViewController: UIViewController, UIGestureRecognizerDelegate, UINaviga
     var specialRotate = false
     var blackBackground = false
     var allowRotate = false
+    var backButton: UIButton?
+    var url = startURL
+    var isMainView = true
+ 
+    
     
     override func loadView() {
         super.loadView()
+        
+        println("LOAD VIEW")
         
         var config = WKWebViewConfiguration()
         config.mediaPlaybackAllowsAirPlay = false;
@@ -55,10 +60,15 @@ class WebViewController: UIViewController, UIGestureRecognizerDelegate, UINaviga
         
         self.view = self.webView
         
-        let recognizer = UILongPressGestureRecognizer(target: self, action:Selector("showBar:"))
-        recognizer.delegate = self
+        let longPress = UILongPressGestureRecognizer(target: self, action:Selector("onLongPress:"))
+        longPress.delegate = self
+        let shortTap = UITapGestureRecognizer(target: self, action:Selector("onShortTap:"))
+        shortTap.delegate = self
         
-        self.view.addGestureRecognizer(recognizer)
+        
+        self.view.addGestureRecognizer(longPress)
+        self.view.addGestureRecognizer(shortTap)
+        shortTap.requireGestureRecognizerToFail(longPress)
         
         self.setUrl(self.url)
         
@@ -67,7 +77,7 @@ class WebViewController: UIViewController, UIGestureRecognizerDelegate, UINaviga
             self.rotate()
         }
         
-        self.setupNavBar()
+        self.createBackButton()
     }
     
     
@@ -121,6 +131,25 @@ class WebViewController: UIViewController, UIGestureRecognizerDelegate, UINaviga
             return true
     }
     
+    func createBackButton() {
+        let image = UIImage(named: "backButton")
+        let button = UIButton.buttonWithType(UIButtonType.Custom) as UIButton
+        var size = image?.size
+        var point = CGPointMake(35, 0);
+        button.frame = CGRect(origin: point, size: size!)
+        button.setImage(image, forState: .Normal)
+        if (self.isMainView) {
+            button.alpha = 0;
+        }
+        button.addTarget(self, action: "popNavigationController:", forControlEvents: UIControlEvents.TouchUpInside)
+        self.backButton = button
+        self.view.addSubview(button)
+    }
+    
+    func popNavigationController(sender: UIButton!) {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    
     func urlFromPart(part: String) -> String {
         var server = "http://localhost:8116"
         var file = "index.html"
@@ -134,9 +163,8 @@ class WebViewController: UIViewController, UIGestureRecognizerDelegate, UINaviga
     }
     
     func setUrlFromPart(part: String) {
-        
         var urlstring = self.urlFromPart(part)
-        
+        self.isMainView = false
         self.setUrl(urlstring)
     }
     
@@ -172,8 +200,6 @@ class WebViewController: UIViewController, UIGestureRecognizerDelegate, UINaviga
         
         // What else?
         return true
-        
-        
     }
     
     func rotate () {
@@ -198,28 +224,30 @@ class WebViewController: UIViewController, UIGestureRecognizerDelegate, UINaviga
         }
     }
     
-//    func fadeBar(to: CGFloat) {
-//        self.fadeBar(to, delay: 0, duration: 0.5)
-//    }
-//
-//    func fadeBar(to: CGFloat, delay: NSTimeInterval) {
-//        self.fadeBar(to, delay: delay, duration: 0.5)
-//    }
-//    
-//    func fadeBar(to: CGFloat, delay: NSTimeInterval, duration: NSTimeInterval) {
-//        println("fading bar");
-//        UIView.beginAnimations(nil, context: nil)
-//        UIView.setAnimationDuration(duration)
-//        UIView.setAnimationDelay(delay)
-//        self.navigationController?.navigationBar.alpha = to
-//        UIView.commitAnimations()
-//        //self.navigationController?.navigationBar.frame = CGRectMake(40, 10, 150, 45)
-//    }
+    func fadeButton(to: CGFloat) {
+        self.fadeButton(to, delay: 0, duration: 0.5)
+    }
+
+    func fadeButton(to: CGFloat, delay: NSTimeInterval) {
+        self.fadeButton(to, delay: delay, duration: 0.5)
+    }
     
-//    func webView(webView: WKWebView!, didFinishNavigation navigation: WKNavigation!) {
-//        println("finished navigation");
-//        self.fadeBar(0, delay: 0, duration: 0)
-//    }
+    func fadeButton(to: CGFloat, delay: NSTimeInterval, duration: NSTimeInterval) {
+        println("fading button")
+        UIView.beginAnimations(nil, context: nil)
+        UIView.setAnimationDuration(duration)
+        UIView.setAnimationDelay(delay)
+        self.backButton?.alpha = to
+        UIView.commitAnimations()
+    }
+    
+    func webView(webView: WKWebView!, didFinishNavigation navigation: WKNavigation!) {
+        println("finished navigation")
+        self.fadeButton(0, delay: 5)
+        // Disable selection
+        webView.evaluateJavaScript("document.documentElement.style.webkitUserSelect='none';", completionHandler: nil);
+        webView.evaluateJavaScript("document.documentElement.style.webkitTouchCallout='none';", completionHandler: nil);
+    }
     
     func webView(webView: WKWebView!, decidePolicyForNavigationAction navigationAction: WKNavigationAction,
         decisionHandler: (WKNavigationActionPolicy) -> Void) {
@@ -245,15 +273,18 @@ class WebViewController: UIViewController, UIGestureRecognizerDelegate, UINaviga
             }
     }
     
-    func setupNavBar() {
-        //self.navigationController?.setNavigationBarHidden(false, animated: true)
-        //self.navigationController?.navigationBar.alpha = 0
+    @IBAction func onLongPress(sender: AnyObject) {
+        println("long press, showing button");
+        self.fadeButton(1)
+        var time = dispatch_time(DISPATCH_TIME_NOW, Int64(10 * Double(NSEC_PER_SEC)))
+        dispatch_after(time, dispatch_get_main_queue(), {
+            self.fadeButton(0)
+        })
     }
     
-    
-    @IBAction func showBar(sender: AnyObject) {
-        println("showing bar");
-        //self.fadeBar(1)
+    @IBAction func onShortTap(sender: AnyObject) {
+        println("short tap")
+        self.fadeButton(0)
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
@@ -263,8 +294,7 @@ class WebViewController: UIViewController, UIGestureRecognizerDelegate, UINaviga
             let specialRotate = dict["specialRotate"] as Bool;
             
             if let destination = segue.destinationViewController as? WebViewController {
-                destination.setUrlFromPart(path);
-                //hadestination.setLandscape(landscape)
+                destination.setUrlFromPart(path)
                 destination.setSpecialRotate(specialRotate)
             }
         }
