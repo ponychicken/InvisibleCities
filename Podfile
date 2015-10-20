8184a86f592dbfe4a9fa7fa7dad0753dc29b7e5c
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-10-20 12:22:24 +0200

    Update CocoaPods

--- Podfile
+++ Podfile
@@ -2,5 +2,4 @@
 # platform :ios, '6.0'
 
 pod "GCDWebServer", "~> 3.0"
-pod 'Reveal-iOS-SDK', :configurations => ['Debug']
 pod 'SwipeBack', '~> 1.0'
--- Pods/Target Support Files/Pods-SwipeBack/Pods-SwipeBack-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwipeBack : NSObject
-@end
-@implementation PodsDummy_Pods_SwipeBack
-@end
